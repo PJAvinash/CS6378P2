6ba0d84fb4f31d88f176d3d3d19a69800baca8be
PJ Avinash <avinashindian2.0@gmail.com>
2024-03-26 17:48:26 -0500

    io debug

--- io.cpp
+++ io.cpp
@@ -62,7 +62,6 @@ int sendMessage(const std::vector<unsigned char> &bytes, const Node &destination
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepDuration));
             if(NUM_ATTEMPTS == (attempt+1)){
                 perror("Error connecting");
-                std::cout << getIPV4(destination.hostname).c_str() << " destination name : " << destination.hostname << " port: " << destination.port << "\n";
                 close(client);
                 return -1;
             }   
