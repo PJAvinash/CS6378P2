93b69859bbf1027e89953d843d8a8ef65d341b07
PJ Avinash <avinashindian2.0@gmail.com>
2024-03-26 20:34:13 -0500

    main test debug

--- main.cpp
+++ main.cpp
@@ -33,107 +33,120 @@ std::string getHostname()
 
 void test(const std::vector<Node> &nodes, int num_keys)
 {
-    printf("#\n");
-    std::vector<ReplicatedKVS<int, int> *> replicatedKVS;
-    std::string host = getHostname() ;
-    for (int i = 0; i < nodes.size(); i++)
+    try
     {
-        
-        if (getHostname() == nodes[i].hostname)
+        printf("#\n");
+        std::vector<ReplicatedKVS<int, int> *> replicatedKVS;
+        std::string host = getHostname();
+        for (int i = 0; i < nodes.size(); i++)
         {
-            ReplicatedKVS<int, int> *rkv = new ReplicatedKVS<int, int>(nodes[i], nodes[0], nodes);
-            replicatedKVS.push_back(rkv);
-            std::cout << rkv->uid() << std::endl;
-        }
-    }
-    int num_nodes = replicatedKVS.size();
-    std::cout << host << " num_nodes: " <<num_nodes <<"\n";
-    sleep(2);
-    for (int i = 0; i < num_keys; i++)
-    {
-        int replicauid = (i % num_nodes);
-        int uid = replicatedKVS[replicauid]->uid();
-        replicatedKVS[replicauid]->set(i, i *(uid+1));
-    }
-    printf("#\n");
-    for (int i = 0; i < replicatedKVS.size(); i++)
-    {
-        std::cout << replicatedKVS[i]->uid() << std::endl;
-    }
-    printf("#\n");
-    int mismatches = 0;
-    for (int i = 0; i < num_keys; i++)
-    {
-        printf("key : %d\n",i);
-        std::set<int> s;
-        for (int r = 0; r < num_nodes; r++)
-        {
-            try
+
+            if (getHostname() == nodes[i].hostname)
             {
-               int localcopy = replicatedKVS[r]->get(i);
-               s.insert(localcopy);
+                ReplicatedKVS<int, int> *rkv = new ReplicatedKVS<int, int>(nodes[i], nodes[0], nodes);
+                replicatedKVS.push_back(rkv);
+                std::cout << rkv->uid() << std::endl;
             }
-            catch(const std::exception& e)
-            {
-                std::cout<< host << " Error at i = " << i <<"\n"; 
-                std::cerr << host << e.what() << '\n';
-            }
-            
-            
         }
-        if (s.size() > 1)
+        int num_nodes = replicatedKVS.size();
+        std::cout << host << " num_nodes: " << num_nodes << "\n";
+        sleep(2);
+        for (int i = 0; i < num_keys; i++)
         {
-            printSet(s);
-            mismatches++;
+            int replicauid = (i % num_nodes);
+            int uid = replicatedKVS[replicauid]->uid();
+            replicatedKVS[replicauid]->set(i, i * (uid + 1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(2));
         }
+        printf("#\n");
+        for (int i = 0; i < replicatedKVS.size(); i++)
+        {
+            std::cout << replicatedKVS[i]->uid() << std::endl;
+        }
+        printf("#\n");
+        int mismatches = 0;
+        for (int i = 0; i < num_keys; i++)
+        {
+            printf("key : %d\n", i);
+            std::set<int> s;
+            for (int r = 0; r < num_nodes; r++)
+            {
+                try
+                {
+                    int localcopy = replicatedKVS[r]->get(i);
+                    s.insert(localcopy);
+                }
+                catch (const std::exception &e)
+                {
+                    std::cout << host << " Error at i = " << i << "\n";
+                    std::cerr << host << e.what() << '\n';
+                }
+            }
+            if (s.size() > 1)
+            {
+                printSet(s);
+                mismatches++;
+            }
+        }
+        printf("#\n");
+        for (int i = 0; i < replicatedKVS.size(); i++)
+        {
+            std::vector<int> keys = replicatedKVS[i]->getkeys();
+            for (int k : keys)
+            {
+                std::cout << "uid: " << replicatedKVS[i]->uid() << " k: " << k << " v: " << replicatedKVS[i]->get(k) << "\n";
+            }
+        }
+        printf("%s: Number mismatches for %d keys: %d\n", host.c_str(), num_keys, mismatches);
     }
-    printf("#\n");
-    for (int i = 0; i < replicatedKVS.size(); i++)
+    catch (const std::exception &e)
     {
-        std::vector <int> keys = replicatedKVS[i]->getkeys();
-        for(int k:keys){
-            std::cout << "uid: "<< replicatedKVS[i]->uid() <<" k: " <<k <<" v: "<< replicatedKVS[i]->get(k) << "\n";
-        }
+        std::cerr << e.what() << '\n';
     }
-    printf("%s: Number mismatches for %d keys: %d\n",host.c_str(), num_keys, mismatches);
 }
 
-std::vector<Node> readNodesFromFile(const std::string& filename) {
-  std::vector<Node> nodes;
-  std::ifstream file(filename);
+std::vector<Node> readNodesFromFile(const std::string &filename)
+{
+    std::vector<Node> nodes;
+    std::ifstream file(filename);
 
-  if (!file.is_open()) {
-    throw std::runtime_error("Error: Unable to open file " + filename);
-  }
-
-  std::string line;
-  while (std::getline(file, line)) {
-    // Ignore comments and empty lines
-    if (line.empty() || line[0] == '#') {
-      continue;
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Error: Unable to open file " + filename);
     }
 
-    std::istringstream iss(line);
-    int uid;
-    std::string hostname;
-    int port;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        // Ignore comments and empty lines
+        if (line.empty() || line[0] == '#')
+        {
+            continue;
+        }
 
-    // Extract and validate data
-    if (!(iss >> uid >> hostname >> port)) {
-      std::cerr << "Error: Invalid line format in file " << filename << std::endl;
-      continue;
+        std::istringstream iss(line);
+        int uid;
+        std::string hostname;
+        int port;
+
+        // Extract and validate data
+        if (!(iss >> uid >> hostname >> port))
+        {
+            std::cerr << "Error: Invalid line format in file " << filename << std::endl;
+            continue;
+        }
+
+        // Basic port validation (adjust as needed)
+        if (port <= 0 || port > 65535)
+        {
+            std::cerr << "Error: Invalid port number in file " << filename << std::endl;
+            continue;
+        }
+
+        nodes.push_back(Node{uid, hostname, port});
     }
-
-    // Basic port validation (adjust as needed)
-    if (port <= 0 || port > 65535) {
-      std::cerr << "Error: Invalid port number in file " << filename << std::endl;
-      continue;
-    }
-
-    nodes.push_back(Node{uid, hostname, port});
-  }
-  file.close();
-  return nodes;
+    file.close();
+    return nodes;
 }
 
 int main(int argc, char *argv[])
@@ -143,7 +156,9 @@ int main(int argc, char *argv[])
         std::vector<Node> nodes = readNodesFromFile(argv[1]);
         int num_keys = atoi(argv[2]);
         test(nodes, num_keys);
-    }else{
+    }
+    else
+    {
         perror("Invalid number of arguments format:'<executable> <config path> <num_keys>'");
     }
     exit(0);
